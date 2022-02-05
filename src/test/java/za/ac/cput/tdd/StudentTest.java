b2227dce7e4904a4ea735947c4d1df34f87b5e61
Monehi Tuoane <monehituoane@gmail.com>
2022-02-05 21:03:15 +0200

    Added TDD applications to studentTest file

--- src/test/java/za/ac/cput/tdd/StudentTest.java
+++ src/test/java/za/ac/cput/tdd/StudentTest.java
@@ -1,10 +1,66 @@
 package za.ac.cput.tdd;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import java.util.concurrent.TimeUnit;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StudentTest {
 
-    @org.junit.jupiter.api.BeforeEach
+
+    //Application declaration for Object Identity and Equality Testing
+    private Student student1;
+    private Student student2;
+    private Student student3;
+
+    @BeforeEach
     void setUp() {
+        student1 = new Student();
+        student2 = new Student();
+        student3 = student1;
     }
+
+    //Application Declaration for Test-Fail calculations
+    private Student calculate;
+    public StudentTest() {
+    }
+
+
+    //Identity Testing
+    @Test
+    void testIdentity () {
+        assertSame(student1, student1);
+    }
+
+    //Equality Testing
+    @Test
+    void testEquality () {
+        assertEquals(student1,student3);
+    }
+
+
+    //Fail Test
+    @Test
+    void testFail() {
+        assertEquals(student1,student3);
+        //fail();
+    }
+
+    //Disabling Test
+
+    @Test
+    @Disabled
+    void testDisabled() {
+        assertSame(student1, student1);
+    }
+
+    @Test
+    @Timeout(value = 500, unit = TimeUnit.MILLISECONDS)
+    void test_this() {
+    }
+
 }
