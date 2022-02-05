81d7971c1b0746e089ec920965213880751e77d8
Monehi Tuoane <monehituoane@gmail.com>
2022-02-05 02:03:17 +0200

    Initial Commit

---
+++ src/main/java/za/ac/cput/tdd/Introduction.java
@@ -0,0 +1,8 @@
+package za.ac.cput.tdd;
+
+public class Introduction {
+    public static void main (String[]args) {
+        System.out.println("Please create a branch with your section of the work");
+        System.out.println("Use your student number as the name of your branch.");
+    }
+}
